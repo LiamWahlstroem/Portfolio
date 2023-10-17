dbede8fe1291a8996897187bac48b4ff5f859d84
Liam <liam.wahlstroem@gmail.com>
2023-10-17 19:54:43 +0200

    Add Lambda function for resizing image
    - [Misc] Add Lambda Function for resizing images

--- Lambda/ImageResize/index.js
+++ Lambda/ImageResize/index.js
@@ -1,5 +1,14 @@
+const sharp = require("sharp");
+
 exports.handler = async (event) => {
-    const image = event.image;
-
-
+    try {
+        const image = event.image;
+        return await sharp(image)
+            .resize(event.sizeX, event.sizeY)
+            .webp({quality: 85})
+            .toBuffer();
+    } catch (error) {
+        console.error(error);
+        throw new Error('Image processing failed.');
+    }
 };
