163d44cdbfd28013193303c4f9d75195fe117b1f
Liam <liam.wahlstroem@gmail.com>
2025-03-05 20:39:26 +0100

    Update gallery view, Dependency Updates

--- photography/components/Molecules/CollectionCard.tsx
+++ photography/components/Molecules/CollectionCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 const CollectionCard = (props: Props): ReactElement => {
 	return (
 		<div
-			className='w-11/12 md:basis-1/4 p-4 border border-gray-300 rounded-lg bg-white shadow-md hover:shadow-lg transition-shadow duration-300 mx-auto md:mx-0 hover:cursor-pointer'
+			className='min-w-1/4 w-full md:basis-1/4 p-4 border border-gray-300 rounded-lg bg-white shadow-md hover:shadow-lg transition-shadow duration-300 mx-auto md:mx-0 hover:cursor-pointer'
 			data-id={props.collectionId}
 			onClick={(ev) => props.onClick(ev)}
 		>
